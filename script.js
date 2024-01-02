0b1c64defa1a353f47bf4a3a204c33907d830307
Voltster <voltster95@gmail.com>
2024-01-02 18:52:27 +0530

    Add existing project files to Git

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+var container = document.querySelector(".elem-container")
+var fixed = document.querySelector(".fixed-image")
+
+container.addEventListener("mouseenter", function(){
+    fixed.style.display = "block"
+})
+
+container.addEventListener("mouseleave", function(){
+    fixed.style.display = "none"
+})
+
+var elems = document.querySelectorAll(".elem")
+
+ elems.forEach(function(e){
+    e.addEventListener("mouseenter",function(){
+        var image = e.getAttribute("data-image")
+        fixed.style.backgroundImage =`url("${image}")` 
+    })
+ })
